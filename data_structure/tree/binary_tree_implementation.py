d30e8206c1899725f1be44744ae52855bddeb40d
jammy25 <kishan95.jammy@gmail.com>
2022-05-15 15:40:14 +0530

    Insertion Operation

--- data_structure/tree/binary_tree_implementation.py
+++ data_structure/tree/binary_tree_implementation.py
@@ -1,17 +1,32 @@
+from curses.ascii import BS
+
+
 class BST:
     def __init__(self, key):
         self.key = key
         self.lchild = None
         self.rchild = None
-root = BST(10)
-print(root.key)
-print(root.lchild)
-print(root.rchild)
 
-root.lchild = BST(5)
-print(root.key)
-print(root.lchild)
-print(root.rchild)
-print(root.lchild.key)
-print(root.lchild.lchild)
-print(root.lchild.rchild)
+# Insertion operation
+
+    def insert(self, data):
+        if self.key is None:                      # for empty tree
+            self.key = data
+            return
+        if self.key == data:                      # for duplicate case
+            return
+        if self.key > data:
+            if self.lchild:
+                self.lchild.insert(data)          # to check which node after root node is empty on left side (Recursion)    
+            else:
+                self.lchild = BST(data)
+        else:
+            if self.rchild:
+                self.rchild.insert(data)
+            else:
+                self.rchild = BST(data)
+
+root = BST(20)
+list1 = [20, 4, 30, 4, 1, 5, 6]
+for i in list1:
+    root.insert(i)
