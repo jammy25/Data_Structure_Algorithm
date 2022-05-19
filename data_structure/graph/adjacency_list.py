ad96c16bf4e568c653c62e96e41bb47394980d13
jammy25 <kishan95.jammy@gmail.com>
2022-05-19 17:56:11 +0530

    add edge for undirected and weighted graph

--- data_structure/graph/adjacency_list.py
+++ data_structure/graph/adjacency_list.py
@@ -8,18 +8,21 @@ def add_node(v):
 
 # add_edge
 
-def add_edge(v1, v2):
+def add_edge(v1, v2, cost):
     if v1 not in graph:
         print(v1, "is not present in the graph.")
     elif v2 not in graph:
         print(v2, "is not present in the graph.")
     else:
-        graph[v2].append(v1)
-        graph[v1].append(v2)
+        list1 = [v1, cost]
+        list2 = [v2, cost]
+        graph[v2].append(list1)
+        graph[v1].append(list2)
 
 graph = {}
 add_node('A')
 add_node('B')
 add_node('C')
-add_edge("A","B")
+add_edge("A", "B", 20)
+add_edge("A", "C", 10)
 print(graph)
