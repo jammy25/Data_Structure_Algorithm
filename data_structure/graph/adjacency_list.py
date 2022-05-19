e317dc7184996fbd3aa50a464531f4b9585cb73c
jammy25 <kishan95.jammy@gmail.com>
2022-05-19 17:46:21 +0530

    add edge for undirected and unfiltered graph

--- data_structure/graph/adjacency_list.py
+++ data_structure/graph/adjacency_list.py
@@ -6,7 +6,20 @@ def add_node(v):
     else:
         graph[v] = []
 
+# add_edge
+
+def add_edge(v1, v2):
+    if v1 not in graph:
+        print(v1, "is not present in the graph.")
+    elif v2 not in graph:
+        print(v2, "is not present in the graph.")
+    else:
+        graph[v2].append(v1)
+        graph[v1].append(v2)
+
 graph = {}
 add_node('A')
 add_node('B')
+add_node('C')
+add_edge("A","B")
 print(graph)
