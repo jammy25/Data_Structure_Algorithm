3cf6b78833e47874b45756989d0cc3798e737a33
jammy25 <kishan95.jammy@gmail.com>
2022-05-21 08:53:59 +0530

    delete node for directed - unweighted graph.

--- data_structure/graph/adjacency_list.py
+++ data_structure/graph/adjacency_list.py
@@ -8,21 +8,34 @@ def add_node(v):
 
 # add_edge
 
-def add_edge(v1, v2, cost):
+def add_edge(v1, v2):
     if v1 not in graph:
         print(v1, "is not present in the graph.")
     elif v2 not in graph:
         print(v2, "is not present in the graph.")
     else:
         # list1 = [v1, cost]
-        list2 = [v2, cost]
+        # list2 = [v2, cost]
         # graph[v2].append(list1)
-        graph[v1].append(list2)
+        graph[v1].append(v2)
+
+# delete_node
+
+def delete_node(v):
+    if v not in graph:
+        print(v, "is not present in the graph.")
+    else:
+        graph.pop(v)
+        for i in graph:
+            list1 = graph[i]
+            if v in list1:
+                list1.remove(v)
 
 graph = {}
 add_node('A')
 add_node('B')
 add_node('C')
-add_edge("A", "B", 20)
-add_edge("A", "C", 10)
+add_edge("A", "B")
+add_edge("A", "C")
+# delete_node("C")
 print(graph)
