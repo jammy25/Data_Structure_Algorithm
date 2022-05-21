93c3eb2affaef302e582b6cdb6bf75562a466ba0
jammy25 <kishan95.jammy@gmail.com>
2022-05-21 09:27:07 +0530

    delete node for directed - weighted

--- data_structure/graph/adjacency_list.py
+++ data_structure/graph/adjacency_list.py
@@ -8,16 +8,16 @@ def add_node(v):
 
 # add_edge
 
-def add_edge(v1, v2):
+def add_edge(v1, v2, cost):
     if v1 not in graph:
         print(v1, "is not present in the graph.")
     elif v2 not in graph:
         print(v2, "is not present in the graph.")
     else:
-        # list1 = [v1, cost]
-        # list2 = [v2, cost]
-        graph[v2].append(v1)
-        graph[v1].append(v2)
+        list1 = [v1, cost]
+        list2 = [v2, cost]
+        graph[v2].append(list1)
+        graph[v1].append(list2)
 
 # delete_node
 
@@ -28,14 +28,19 @@ def delete_node(v):
         graph.pop(v)
         for i in graph:
             list1 = graph[i]
-            if v in list1:
-                list1.remove(v)
+            # if v in list1:
+                # list1.remove(v)
+            for j in list1:
+                if v == j[0]:
+                    list1.remove(j)
+                    break
+
 
 graph = {}
 add_node('A')
 add_node('B')
 add_node('C')
-add_edge("A", "B")
-add_edge("A", "C")
+add_edge("A", "B", 20)
+add_edge("A", "C", 5)
 delete_node("C")
 print(graph)
