0a937f06732cfaab3548f3feceb770112a30095a
jammy25 <kishan95.jammy@gmail.com>
2022-05-21 07:28:49 +0530

    delete edge operation for undirected weighted/unweighted graph.

--- data_structure/graph/adjaceny_matrix.py
+++ data_structure/graph/adjaceny_matrix.py
@@ -18,7 +18,7 @@ def add_node(v):
 
 def add_edge(v1, v2, cost):
     if v1 not in nodes:
-        print (v1, "is not present in the graph.")
+        print (v1, "is not present in the graph!")
     elif v2 not in nodes:
         print (v2, "is not present in the graph.")
     else:
@@ -32,7 +32,7 @@ def add_edge(v1, v2, cost):
 def delete_node(v):
     global node_count
     if v not in nodes:
-        print(v, "is not present in the graph.")
+        print(v, "is not present in the graph!!")
     else:
         index1 = nodes.index(v)
         node_count = node_count - 1
@@ -40,6 +40,20 @@ def delete_node(v):
         graph.pop(index1)
         for i in graph:
             i.pop(index1)
+
+#delete_edge
+
+def delete_edge(v1, v2):
+    if v1 not in nodes:
+        print(v1, "is not present in the graph.")
+    elif v2 not in nodes:
+        print(v2, "is not present in the graph.")  
+    else:
+        index1 = nodes.index(v1)
+        index2 = nodes.index(v2)
+        graph[index1][index2] = 0
+        graph[index2][index1] = 0
+        
 
 # to print in matrix form
 def print_graph():
@@ -55,13 +69,14 @@ graph = []
 node_count = 0
 add_node("A")
 add_node("B")
-# add_node("C")
+add_node("C")
 # print(node_count)
 # print("After adding nodes")
 # print(nodes)
 add_edge("A", "B", 10)
-# add_edge("A", "C", 5)
-delete_node("A")
+add_edge("A", "C", 5)
+print_graph()
+delete_edge("A", "C")
 print("graph after deleting:")
 print_graph()
 print("nodes: ", nodes)
